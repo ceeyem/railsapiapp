50377df5038e0d1be41d2376890f8e3303bc4647
CEEYEM <ceeyem@madavoor>
2023-12-02 23:45:45 +0530

    up

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,18 +1,11 @@
 class Api::V1::QuestionsController < ApplicationController
-  before_action :set_question, only: %i[ show update destroy ]
-  before_action :require_login,  only: %i[ index ]
+  before_action :require_login,  only: %i[ index, create ]
 
 
   # GET /questions
   def index
-    @questions = Question.order(created_at: :desc)
+    @questions = Question.order(ask_count: :desc)
     render json: @questions
-  end
-
-  # GET /questions/1
-  def show
-    sleep 3
-    render json: @question
   end
 
   # POST /questions
@@ -25,26 +18,7 @@ class Api::V1::QuestionsController < ApplicationController
       render json: @question.errors, status: :unprocessable_entity
     end
   end
-
-  # PATCH/PUT /questions/1
-  def update
-    if @question.update(question_params)
-      render json: @question
-    else
-      render json: @question.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /questions/1
-  def destroy
-    @question.destroy!
-  end
-
   private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_question
-    @question = Question.find(params[:id])
-  end
 
   # Only allow a list of trusted parameters through.
   def question_params
